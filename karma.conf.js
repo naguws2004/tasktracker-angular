fe4c7431d8e9406cb684a0ce301ffe1630f6a0c7
Nagesh Kumar Y <nageshkumar.y@gmail.com>
2022-04-08 11:04:51 +0530

    first commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-crash'),
+      dir: require('path').join(__dirname, './coverage/angular-task-tracker'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-crash'`, () => {
+  it(`should have as title 'angular-task-tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-crash');
+    expect(app.title).toEqual('angular-task-tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-crash app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-task-tracker app is running!');
   });
 });
